8abfd85f0f581b25cddbe1be2030a8c6c61f5893
gax <gax@disroot.org>
2023-02-17 14:39:18 -0300

    WLAN 6a

--- boot.py
+++ boot.py
@@ -1,1 +1,9 @@
-# boot.py -- run on boot-up
+import network
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print('conectando a la red...')
+    wlan.connect('SSID', 'PASSWORD')
+    while not wlan.isconnected():
+        pass
--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from machine import Pin
-import time
-
-led = Pin(2, Pin.OUT)
-while (True):
-    led.on()
-    print("encender")
-    time.sleep(.5)
-    led.off()
-    print("apagar")
-    time.sleep(.5)
+print('Datos de la red: ', wlan.ifconfig())
