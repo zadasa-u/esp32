8382db77035e80cedb84e92cca5b1a358f5f88d0
Germán Xander <german.xander@fio.unam.edu.ar>
2024-03-19 15:47:10 -0300

    bandera

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ print("esperando pulsador")
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
+contador=0
+bandera=True
 
 while True:
-    if sw.value():
+    if sw.value() and bandera:
+        bandera=False
         led.value(not led.value())
+        contador += 1
+        print(contador)
+    elif not sw.value():
+        bandera=True
     time.sleep_ms(5)
