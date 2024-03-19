d723fee705090ffd4dc446bcb179b10ff637f433
Germán Xander <german.xander@fio.unam.edu.ar>
2024-03-19 16:27:03 -0300

    settings

--- boot.py
+++ boot.py
@@ -1,9 +1,10 @@
 import network
+from settings import SSID, password
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('conectando a la red...')
-    wlan.connect('SSID', 'PASSWORD')
+    wlan.connect(SSID, password)
     while not wlan.isconnected():
         pass
