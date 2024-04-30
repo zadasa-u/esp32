20737466765e6bfb5ced006c782976ba166bf40b
GermanXander <128548181+GermanXander@users.noreply.github.com>
2024-04-30 16:49:35 -0300

    sin user/pass

--- lib/mqtt_local.py
+++ lib/mqtt_local.py
@@ -1,7 +1,7 @@
 # mqtt_local.py Local configuration for mqtt_as demo programs.
 from sys import platform, implementation
 from mqtt_as import config
-from settings import BROKER, SSID, PASS
+from settings import BROKER
 # , USR_MQTT, PASS_MQTT
 
 #  config['server'] = '192.168.0.10'  # Change to suit
